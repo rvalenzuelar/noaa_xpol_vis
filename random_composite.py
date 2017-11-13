7456791f9fb397ab16a35dfa1745672b4740f016
Raul Valenzuela <raul.valenzuela@colorado.edu>
2017-11-13 09:36:03 -0300

    first commit

---
+++ random_composite.py
@@ -0,0 +1,356 @@
+
+from make_xarray import make_xarray_rhi
+from make_xarray import make_xarray_ppi
+import xpol_tta_analysis as xta
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import xpol
+
+
+cmap_rhi = xpol.custom_cmap('rhi_vr1')
+cmap_ppi = xpol.custom_cmap('ppi_vr1')
+
+params = dict(wdir_thres=150,
+              rain_czd=0.25,
+              nhours=2
+              )
+
+
+def random_sample(xpset,verbose=True):
+
+    max_sample = np.min([x.time.size for x in xpset])
+
+    first = True
+    for ds in xpset:
+        # rnd = np.random.randint(0, ds.time.size, max_sample)
+        rnd = random.sample(range(ds.time.size), max_sample)
+        print 'rnd={}'.format(rnd)
+        if first:
+            conVR = ds['VR'].isel(time=rnd)
+            conZA = ds['ZA'].isel(time=rnd)
+            first = False
+        else:
+            conVR = xr.concat([conVR, ds['VR'].isel(time=rnd)],
+                            dim='time')
+            conZA = xr.concat([conZA, ds['ZA'].isel(time=rnd)],
+                            dim='time')
+
+    if verbose:
+        print 'max_sample:{}'.format(max_sample)
+        print 'total N:{}'.format(conVR.time.size)
+
+    return conVR, conZA
+
+
+
+def rnd_rhi_tta():
+
+    ''' case 12 excluded because RHI scan is incomplete '''
+
+    ds08 = make_xarray_rhi(xp08,regime='tta')
+    ds09 = make_xarray_rhi(xp09,regime='tta')
+    ds11 = make_xarray_rhi(xp11,regime='tta')
+    ds13 = make_xarray_rhi(xp13,regime='tta')
+
+    con = xr.concat([ds08, ds09], dim='time')
+    con = xr.concat([con, ds11], dim='time')
+    con = xr.concat([con, ds13], dim='time')
+
+    rnd = np.random.randint(0, 134, 68)
+    sample = con['VR'].isel(time=rnd)
+    fig,ax = plt.subplots()
+    sample.mean(dim='time').plot(ax=ax, levels=9,
+                                 vmin=0,vmax=20)
+
+def rnd_rhi(reps=None, regime=None):
+
+    ''' case 12 excluded because RHI scan is incomplete '''
+
+    if regime == 'tta':
+        ds08 = make_xarray_rhi(xp08,regime='tta')
+        ds09 = make_xarray_rhi(xp09,regime='tta')
+        ds11 = make_xarray_rhi(xp11,regime='tta')
+        ds13 = make_xarray_rhi(xp13,regime='tta')
+        xpset = [ds08, ds09, ds11, ds13]
+
+    elif regime == 'ntta':
+        ds08 = make_xarray_rhi(xp08, regime='ntta')
+        ds09 = make_xarray_rhi(xp09, regime='ntta')
+        ds10 = make_xarray_rhi(xp10, regime='ntta')
+        ds11 = make_xarray_rhi(xp11, regime='ntta')
+        ds13 = make_xarray_rhi(xp13, regime='ntta')
+        ds14 = make_xarray_rhi(xp14, regime='ntta')
+        xpset = [ds08, ds09, ds10, ds11, ds13, ds14]
+
+
+    fig, ax = plt.subplots(2, 1, sharex=True)
+
+    if reps is None:
+        rnd_VR, rnd_ZA = random_sample(xpset)
+        rnd_VR.mean(dim='time').plot(ax=ax[0], levels=9,
+                                     vmin=0,vmax=20)
+        median = rnd_ZA.median()
+        print 'median={}'.format(median.values)
+
+        enh = rnd_ZA.where(rnd_ZA > median).count(
+            dim='time').astype(float)
+        all = rnd_ZA.count(dim='time').astype(float)
+        frac = enh / all
+
+        frac.where(frac < 1).plot(ax=ax[1], vmin=0, vmax=1)
+
+    else:
+        first = True
+        nreps = reps
+        while reps > 0:
+
+            print reps
+            rnd_VR, rnd_ZA = random_sample(xpset,verbose=True)
+            one_mean = rnd_VR.mean(dim='time')
+            median = rnd_ZA.median()
+            print 'median={}'.format(median.values)
+            enh = rnd_ZA.where(rnd_ZA > median).count(
+                dim='time').astype(float)
+            all = rnd_ZA.count(dim='time').astype(float)
+            frac = enh / all
+            one_frac = frac.where(frac < 1)
+
+            if first:
+                con_mean = one_mean.copy()
+                con_frac = one_frac.copy()
+                first = False
+            else:
+                con_mean = xr.concat([con_mean, one_mean])
+                con_frac = xr.concat([con_frac, one_frac])
+
+            reps -= 1
+
+        mean = con_mean.mean(dim='concat_dims')
+        frac = con_frac.mean(dim='concat_dims')*100
+
+        ''' radial distance for masking '''
+        rho = np.sqrt(frac.x ** 2 + frac.z ** 2)  # [km]
+
+        m = mean.where(rho<28).plot.contourf(ax=ax[0],
+                                levels=16, vmin=0, vmax=30,
+                                add_labels=True,
+                                cmap=cmap_rhi)
+        f = frac.where(rho<28).plot.contourf(ax=ax[1], cmap='inferno',
+                                levels=9,
+                                vmin=20, vmax=100)
+
+        ax[0].text(-40, 13, 'repetitions: {}'.format(nreps),
+                   ha='left')
+
+        ax[0].set_xlim([-40,40])
+        ax[1].set_xlim([-40,40])
+        ax[0].set_ylim([0, 5])
+        ax[1].set_ylim([0, 5])
+
+        m.colorbar.set_label('VR')
+        f.colorbar.set_label('FREQ[%]')
+
+
+def rnd_rhi_ntta():
+
+    ''' case 12 excluded because RHI scan is incomplete '''
+
+    ds08 = make_xarray_rhi(xp08,regime='ntta')
+    ds09 = make_xarray_rhi(xp09,regime='ntta')
+    ds10 = make_xarray_rhi(xp10, regime='ntta')
+    ds11 = make_xarray_rhi(xp11,regime='ntta')
+    ds13 = make_xarray_rhi(xp13,regime='ntta')
+    ds14 = make_xarray_rhi(xp14, regime='ntta')
+
+    con = xr.concat([ds08, ds09], dim='time')
+    con = xr.concat([con, ds10], dim='time')
+    con = xr.concat([con, ds11], dim='time')
+    con = xr.concat([con, ds13], dim='time')
+    con = xr.concat([con, ds14], dim='time')
+
+    rnd = np.random.randint(0, 632, 316)
+    sample = con['VR'].isel(time=rnd)
+    fig,ax = plt.subplots()
+    sample.mean(dim='time').plot(ax=ax, levels=9,
+                                 vmin=0,vmax=20)
+
+
+
+def rnd_ppi_tta():
+
+    ds08 = make_xarray_ppi(xp08, regime='tta')
+    ds09 = make_xarray_ppi(xp09, regime='tta')
+    ds11 = make_xarray_ppi(xp11, regime='tta')
+    ds12 = make_xarray_ppi(xp12, regime='tta')
+    ds13 = make_xarray_ppi(xp13, regime='tta')
+
+    con = xr.concat([ds08, ds09], dim='time')
+    con = xr.concat([con, ds11], dim='time')
+    con = xr.concat([con, ds12], dim='time')
+    con = xr.concat([con, ds13], dim='time')
+
+    rnd = np.random.randint(0, 292, 146)
+    sample = con['VR'].isel(time=rnd)
+    fig,ax = plt.subplots()
+    sample.mean(dim='time').plot(ax=ax, levels=13,
+                                 )
+
+def rnd_ppi(reps=None, regime=None):
+
+    ''' case 12 excluded because RHI scan is incomplete '''
+
+    if regime == 'tta':
+
+        ds08 = make_xarray_ppi(xp08,regime='tta')
+        ds09 = make_xarray_ppi(xp09,regime='tta')
+        ds11 = make_xarray_ppi(xp11,regime='tta')
+        ds13 = make_xarray_ppi(xp13,regime='tta')
+        xpset = [ds08, ds09, ds11, ds13]
+
+    elif regime == 'ntta':
+
+        ds08 = make_xarray_ppi(xp08, regime='ntta')
+        ds09 = make_xarray_ppi(xp09, regime='ntta')
+        ds10 = make_xarray_ppi(xp10, regime='ntta')
+        ds11 = make_xarray_ppi(xp11, regime='ntta')
+        ds13 = make_xarray_ppi(xp13, regime='ntta')
+        ds14 = make_xarray_ppi(xp14, regime='ntta')
+        xpset = [ds08, ds09, ds10, ds11, ds13, ds14]
+
+    fig, ax = plt.subplots(2,1, figsize=[4.38,  7.13]
+                                ,sharex=True)
+
+    nreps=reps
+
+    if reps is None:
+        rnd_VR, rnd_ZA = random_sample(xpset)
+        rnd_VR.mean(dim='time').plot(ax=ax[0], levels=13,
+                                     vmin=-24, vmax=24,
+                                     cmap='RdBu')
+
+        median = rnd_ZA.median()
+        print 'median={}'.format(median.values)
+
+        enh = rnd_ZA.where(rnd_ZA > median).count(
+            dim='time').astype(float)
+        all = rnd_ZA.count(dim='time').astype(float)
+        frac = enh / all
+
+        frac = frac*100
+        frac.name = 'FREQ[%]'
+
+        frac.where(frac < 100).plot(ax=ax[1], vmin=20, vmax=80)
+        ax[0].text(-50, 40, 'repetitions: {}'.format(1),ha='left')
+
+    else:
+        first = True
+        while reps > 0:
+
+            print reps
+            rnd_VR, rnd_ZA = random_sample(xpset,verbose=False)
+            one_mean = rnd_VR.mean(dim='time')
+            median = rnd_ZA.median()
+            print 'median={}'.format(median.values)
+            enh = rnd_ZA.where(rnd_ZA > median).count(
+                dim='time').astype(float)
+            all = rnd_ZA.count(dim='time').astype(float)
+            frac = enh / all
+            one_frac = frac.where(frac < 1)
+
+            if first:
+                con_mean = one_mean.copy()
+                con_frac = one_frac.copy()
+                first = False
+            else:
+                con_mean = xr.concat([con_mean, one_mean])
+                con_frac = xr.concat([con_frac, one_frac])
+
+            reps -= 1
+
+        mean = con_mean.mean(dim='concat_dims')
+        frac = con_frac.mean(dim='concat_dims')*100
+        frac.name = 'FREQ[%]'
+
+        ''' radial distance for masking '''
+        rho = np.sqrt(frac.x ** 2 + frac.y ** 2) # [km]
+
+        ''' add radials '''
+        locations = [(-x, -x) for x in range(10,30,5)+[35, 40]]
+        for a in ax:
+            rc = rho.plot.contour(ax=a,levels=range(0, 70, 10),
+                             colors='k', alpha=0.6)
+            plt.clabel(rc,fmt='%1.0f',manual=locations)
+
+        ''' make plots '''
+        m=mean.where(rho<55).plot.contourf(ax=ax[0], levels=16,
+                                           vmin=-30, vmax=30,
+                                            cmap=cmap_ppi)
+        f=frac.where(rho<55).plot.contourf(ax=ax[1], vmin=20,
+                                           vmax=100,
+                           cmap='inferno',
+                                levels=9,)
+        ax[0].text(-50, 50, 'repetitions: {}'.format(nreps),
+                   ha='left')
+
+        ax[0].axhline(0, color='k', alpha=0.6)
+        ax[0].axvline(0, color='k', alpha=0.6)
+
+        ax[1].axhline(0, color='k', alpha=0.6)
+        ax[1].axvline(0, color='k', alpha=0.6)
+
+        m.colorbar.set_label('VR')
+        f.colorbar.set_label('FREQ[%]')
+
+        plt.tight_layout()
+        plt.subplots_adjust(top=0.95)
+
+
+if __name__ == '__main__':
+
+    try:
+        xp08
+    except NameError:
+        xp08 = xta.process(case=[8], params=params)
+
+    try:
+        xp09
+    except NameError:
+        xp09 = xta.process(case=[9], params=params)
+
+    try:
+        xp10
+    except NameError:
+        xp10 = xta.process(case=[10], params=params)
+
+    try:
+        xp11
+    except NameError:
+        xp11 = xta.process(case=[11], params=params)
+
+    try:
+        xp12
+    except NameError:
+        xp12 = xta.process(case=[12], params=params)
+
+    try:
+        xp13
+    except NameError:
+        xp13 = xta.process(case=[13], params=params)
+
+    try:
+        xp14
+    except NameError:
+        xp14 = xta.process(case=[14], params=params)
+
+
+    # rnd_rhi(reps=2, regime='tta')
+    # rnd_rhi(reps=2, regime='ntta')
+
+
+    # rnd_ppi(regime='tta')
+    # rnd_ppi(regime='ntta')
+
+    rnd_ppi(reps=100,regime='tta')
+    rnd_ppi(reps=100,regime='ntta')
